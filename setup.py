bc106d535810537d5987538f43b40e297abe5332
LRB411-Win10 <yll0620@gmail.com>
2019-06-07 14:29:56 -0500

    add setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 # Always prefer setuptools over distutils
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name                  =   'import_nbmodule',
-      version               =   '0.0.1',
-      description           =   'import jupyter notebook .ipynb module',
-      long_description      =   '',
-      
-      py_modules            =   ['importnb'],
-      
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+
+setup(name                  		=   'import_nbmodule',
+      version               		=   '0.0.1',
+      description           		=   'import jupyter notebook .ipynb module',
+      long_description      		=   long_description,
+      long_description_content_type	=	"text/markdown",
+      author 						= 	"Lingling Yang",
+      author_email 					= 	"yll0620@gmail.com",
+      py_modules            		=   ['import_nbmodule'],
+      packages 						= 	find_packages(),
       # the project's homepage
-      url                   =   'https://github.com/yangll0620/importnb',
-      author                =   'Lingling Yang'
-      
+      url                   		=   'https://github.com/yangll0620/import_nbmodule',
+      classifiers					= [
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      ],
 )
-
